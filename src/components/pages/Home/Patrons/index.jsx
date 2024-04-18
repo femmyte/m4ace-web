a3f9b6923a701b92ceed8680d3372371762f59de
femmyte <sanyaoluadefemi@gmail.com>
2024-04-18 11:02:07 +0100

    Adjusted the patron component

--- src/components/pages/Home/Patrons/index.jsx
+++ src/components/pages/Home/Patrons/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import PatronCard from "./PatronCard";
+import Animation from "@/components/common/Animation";
 
 const Patron = () => {
   return (
-    <div className="w-full md:px-[120px] px-4 py-[30px] md:h-screen">
+    <div className="w-full md:px-[120px] px-4 py-[50px] md:h-screen">
       <div className="">
         <h3 className="font-[700] leading-[42px] text-[30px] text-[#222057] text-center">
           Meet our <span className="text-primary">Patrons</span>
@@ -12,42 +13,50 @@ const Patron = () => {
           Our Patrons are made up of tech expert from various field
         </p>
       </div>
-      <div className="flex flex-col md:flex-row items-center md:justify-between gap-y-8 h-full">
-        <PatronCard
-          color={"black"}
-          img={"m4ace-patron-jonathan-ayodele.png"}
-          name={"Jonathan Ayodele "}
-          title={"Cybersecurity Professional"}
-          linkedin={"https://linkedin.com/in/jonatayo"}
-          x={"https://x.com/Jonat_ayo"}
-          facebook={""}
-        />
-        <PatronCard
-          name={"Jude Agboola"}
-          img={"marvin.jpeg"}
-          title={"Full Stack Software Engineer"}
-          linkedin={"https://www.linkedin.com/in/jude-agboola/"}
-          x={"https://twitter.com/MarvinJudeHK"}
-          facebook={""}
-        />
-        <PatronCard
-          color={"white"}
-          img={"rotimi.jpeg"}
-          name={"Oluwarotimi Owolabi "}
-          title={"DevOps at Access Bank Plc"}
-          linkedin={"https://www.linkedin.com/in/timibreez/"}
-          x={"https://x.com/Jonat_ayo"}
-          facebook={""}
-        />
-        <PatronCard
-          color={"black"}
-          img={"akorede.jpg"}
-          name={"Adigun Rasheed "}
-          title={"Website Developer"}
-          linkedin={"https://www.linkedin.com/in/akotechonline/"}
-          x={"https://x.com/Jonat_ayo"}
-          facebook={""}
-        />
+      <div className="flex flex-col md:flex-row items-center md:items-start md:justify-between gap-y-8 h-full">
+        <Animation style="fade-right" placement="center-center">
+          <PatronCard
+            color={"black"}
+            img={"m4ace-patron-jonathan-ayodele.png"}
+            name={"Jonathan Ayodele "}
+            title={"Cybersecurity Professional"}
+            linkedin={"https://linkedin.com/in/jonatayo"}
+            x={"https://x.com/Jonat_ayo"}
+            facebook={""}
+          />
+        </Animation>
+        <Animation style="fade-right" placement="center-center">
+          <PatronCard
+            name={"Jude Agboola"}
+            img={"marvin.jpeg"}
+            title={"Full Stack Software Engineer"}
+            linkedin={"https://www.linkedin.com/in/jude-agboola/"}
+            x={"https://twitter.com/MarvinJudeHK"}
+            facebook={""}
+          />
+        </Animation>
+        <Animation style="fade-left" placement="center-center">
+          <PatronCard
+            color={"white"}
+            img={"rotimi.jpeg"}
+            name={"Oluwarotimi Owolabi "}
+            title={"DevOps at Access Bank Plc"}
+            linkedin={"https://www.linkedin.com/in/timibreez/"}
+            x={"https://x.com/Jonat_ayo"}
+            facebook={""}
+          />
+        </Animation>
+        <Animation style="fade-left" placement="center-center">
+          <PatronCard
+            color={"black"}
+            img={"akorede.jpg"}
+            name={"Adigun Rasheed "}
+            title={"Website Developer"}
+            linkedin={"https://www.linkedin.com/in/akotechonline/"}
+            x={"https://x.com/Jonat_ayo"}
+            facebook={""}
+          />
+        </Animation>
       </div>
     </div>
   );
